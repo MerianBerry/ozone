44e7774177864ab051ec778673ad9f3553aa8d1f
MerianBerry <spcstanley5150@gmail.com>
2023-10-22 11:52:21 -0700

    funny

---
+++ src/scripts/theme/sulfur.lua
@@ -0,0 +1,16 @@
+oz = require "ozone"
+
+local palette = {
+  bg0 = "#2b2823",
+  bg1 = "#24221f",
+  fg0 = "#c9b9a1",
+  fg1 = "#3c3e40",
+}
+
+local M = {
+  name = "Sulfur",
+  Normal = {fg = palette.fg0, bg = palette.bg0},
+  Line = {fg = palette.fg1, bg = palette.bg0},
+}
+
+return M
